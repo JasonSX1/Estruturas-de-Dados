ccb8eb4685486182d753b31833603dc623455481
Geison de Oliveira <geisonyt3@gmail.com>
2024-05-10 09:22:03 -0300

    -

--- TrabalhoED-GUI/src/main/java/com/example/trabalhoedgui/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.trabalhoedgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-    opens com.example.trabalhoedgui to javafx.fxml;
-    exports com.example.trabalhoedgui;
-}
