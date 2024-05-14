484f4ac6e15559f4a7ae1e8b6293e0cef34829b3
Geison de Oliveira <geisonyt3@gmail.com>
2024-05-14 10:44:53 -0300

    Correção de conexão e impedimento de cadastro de usuário duplicado

--- CinemaManagementSystem/src/main/kotlin/com/ed/cinemamanagementsystem/LoginAndSignupController.kt
+++ CinemaManagementSystem/src/main/kotlin/com/ed/cinemamanagementsystem/LoginAndSignupController.kt
@@ -55,174 +55,138 @@ class LoginAndSignupController {
 
     private lateinit var alert: Alert
 
-    fun regBtn(){
-
-        if(su_username.text.isEmpty() || su_password.text.isEmpty() ||  su_question.selectionModel.selectedItem == null || su_answer.text.isEmpty()){
+    fun regBtn() {
+        if (su_username.text.isEmpty() || su_password.text.isEmpty() || su_question.selectionModel.selectedItem == null || su_answer.text.isEmpty()) {
             alert = Alert(Alert.AlertType.ERROR)
             alert.title = "Mensagem de erro!"
             alert.headerText = null
             alert.contentText = "Preencha todos os campos em branco!"
             alert.showAndWait()
-        } else {
-            val regData : String = "INSERT INTO users (username, password, question, answer) VALUES(?, ?, ?, ?)"
-            val connect: Connection? = Database.connectDB()
-            if (connect != null) {
-
-<<<<<<< Updated upstream
-            try {
-
-                val prepare = connect!!.prepareStatement(regData)
-                prepare.setString(1, su_username.text)
-                prepare.setString(2, su_password.text)
-                prepare.setString(3, su_question.selectionModel.selectedItem as String)
-                prepare.setString(4, su_answer.text)
-
-                prepare.executeUpdate()
-
-                alert = Alert(Alert.AlertType.INFORMATION)
-                alert.title = "Mensagem de informação!"
-                alert.headerText = null
-                alert.contentText = "Cadastro bem sucedido!"
-                alert.showAndWait()
-
-                su_username.text = null
-                su_password.text = null
-                su_question.selectionModel.clearSelection()
-                su_answer.text = null
-
-                val slider = TranslateTransition()
-
-                slider.node = side_form
-                slider.toX = 0.0
-                slider.duration = Duration.seconds(0.5)
-
-                slider.setOnFinished {
-                    side_alreadyHave?.isVisible = false
-                    side_CreateBtn?.isVisible = true
-                    AnchorPane.setRightAnchor(side_form, null)
-                }
-
-                slider.play()
-
-
-            }catch (e: Exception){e.printStackTrace()}
-=======
-                try {
-                    //Verificação se o username do usuario foi cadastrado
-                    val checkUsername = "SELECT username FROM users WHERE username = ?"
-                    val prepare = connect.prepareStatement(checkUsername)
-                    prepare.setString(1, su_username.text)
-                    val result = prepare.executeQuery()
-
-                    if(result.next()){
-                        alert = Alert(Alert.AlertType.ERROR)
-                        alert.title = "Mensagem de erro!"
-                        alert.headerText = null
-                        alert.contentText = su_username.text + " já está cadastrado!"
-                        alert.showAndWait()
-                    } else if(su_password.text.length < 8){
-                        alert = Alert(Alert.AlertType.ERROR)
-                        alert.title = "Mensagem de erro!"
-                        alert.headerText = null
-                        alert.contentText = "Sua senha deve conter mais de 8 caracteres!"
-                        alert.showAndWait()
-                    } else {
-
-                        val prepare = connect!!.prepareStatement(regData)
-                        prepare.setString(1, su_username.text)
-                        prepare.setString(2, su_password.text)
-                        prepare.setString(3, su_question.selectionModel.selectedItem as String)
-                        prepare.setString(4, su_answer.text)
-
-                        val date = java.util.Date()
-                        val sqlDate = java.sql.Date(date.time)
-                        prepare.setString(5, sqlDate.toString())
-                        prepare.executeUpdate()
-
-                        alert = Alert(Alert.AlertType.INFORMATION)
-                        alert.title = "Mensagem de informação!"
-                        alert.headerText = null
-                        alert.contentText = "Cadastro bem sucedido!"
-                        alert.showAndWait()
-
-                        su_username.text = null
-                        su_password.text = null
-                        su_question.selectionModel.clearSelection()
-                        su_answer.text = null
-
-                        val slider = TranslateTransition()
-
-                        slider.node = side_form
-                        slider.toX = 0.0
-                        slider.duration = Duration.seconds(0.5)
-
-                        slider.setOnFinished {
-                            side_alreadyHave?.isVisible = false
-                            side_CreateBtn?.isVisible = true
-                            AnchorPane.setRightAnchor(side_form, null)
-                        }
-
-                        slider.play()
-
-                    }
-
-                    if(su_password.text.length < 8){
-                        alert = Alert(Alert.AlertType.ERROR)
-                        alert.title = "Mensagem de erro!"
-                        alert.headerText = null
-                        alert.contentText = "Sua senha deve conter mais de 8 caracteres!"
-                        alert.showAndWait()
-                    }
-                }catch (e: Exception){e.printStackTrace()}
-            } else {
-                //TODO CASE CONNECTION IS NULL
-            }
->>>>>>> Stashed changes
-        }
-    }
-
-    fun regQuestionList(){
-        val listQuestion = ArrayList<String>()
-
-        for (data in questionList) {
-            listQuestion.add(data)
+            return
         }
 
-        val listData: ObservableList<String> = FXCollections.observableArrayList(listQuestion)
-        su_question.items = listData
+        // Verificar se a senha tem pelo menos 8 caracteres
+        if (su_password.text.length < 8) {
+            alert = Alert(Alert.AlertType.ERROR)
+            alert.title = "Mensagem de erro!"
+            alert.headerText = null
+            alert.contentText = "Sua senha deve conter mais de 8 caracteres!"
+            alert.showAndWait()
+            return // Retorna se a senha for muito curta
+        }
 
-    }
+        // Conexão com o banco de dados
+        val connect: Connection? = Database.connectDB()
+        if (connect == null) {
+            // TODO: Lidar com a situação em que a conexão é nula
+            return
+        }
 
-    fun switchForm(event: ActionEvent) {
-        val slider = TranslateTransition()
+        // Verificar se o nome de usuário já existe
+        val checkUsernameQuery = "SELECT username FROM users WHERE username = ?"
+        val checkUsernameStatement = connect.prepareStatement(checkUsernameQuery)
+        checkUsernameStatement.setString(1, su_username.text)
+        val existingUserResult = checkUsernameStatement.executeQuery()
+        if (existingUserResult.next()) {
+            // Caso o nome de usuário já exista:
+            alert = Alert(Alert.AlertType.ERROR)
+            alert.title = "Mensagem de erro!"
+            alert.headerText = null
+            alert.contentText = "O usuário: ${su_username.text} já está cadastrado!"
+            alert.showAndWait()
+            return
+        }
 
-        if (event.source == side_CreateBtn) {
-            slider.node = side_form
-            slider.toX = 300.0
-            slider.duration = Duration.seconds(0.5)
+        // Nome de usuário não duplicado, então podemos prosseguir com o cadastro
+        val regData: String = "INSERT INTO users (username, password, question, answer) VALUES(?, ?, ?, ?)"
+        try {
+            val prepare = connect.prepareStatement(regData)
+            prepare.setString(1, su_username.text)
+            prepare.setString(2, su_password.text)
+            prepare.setString(3, su_question.selectionModel.selectedItem as String)
+            prepare.setString(4, su_answer.text)
+            prepare.executeUpdate()
 
-            slider.setOnFinished {
-                side_alreadyHave?.isVisible = true
-                side_CreateBtn?.isVisible = false
-                AnchorPane.setRightAnchor(side_form, 300.0)
+            alert = Alert(Alert.AlertType.INFORMATION)
+            alert.title = "Mensagem de informação!"
+            alert.headerText = null
+            alert.contentText = "Cadastro bem sucedido!"
+            alert.showAndWait()
 
-                regQuestionList();
-            }
+            su_username.text = null
+            su_password.text = null
+            su_question.selectionModel.clearSelection()
+            su_answer.text = null
 
-            slider.play()
-        } else if (event.source == side_alreadyHave) {
+            val slider = TranslateTransition()
             slider.node = side_form
             slider.toX = 0.0
             slider.duration = Duration.seconds(0.5)
-
             slider.setOnFinished {
                 side_alreadyHave?.isVisible = false
                 side_CreateBtn?.isVisible = true
-                AnchorPane.setRightAnchor(side_form, null)
+                AnchorPane.setRightAnchor(side_form, null
+)
             }
-
             slider.play()
+        } catch (e: SQLException) {
+            // Trata exceções de SQL, como nome de usuário duplicado
+            alert = Alert(Alert.AlertType.ERROR)
+            alert.title = "Mensagem de erro!"
+            alert.headerText = null
+            alert.contentText = "${su_username.text} já está cadastrado!"
+            alert.showAndWait()
+        } finally {
+            // Fecha a conexão com o banco de dados
+            try {
+                connect.close()
+            } catch (e: SQLException) {
+                // Lidar com exceções ao fechar a conexão
+                e.printStackTrace()
+            }
         }
     }
+            fun regQuestionList() {
+                val listQuestion = ArrayList<String>()
 
+                for (data in questionList) {
+                    listQuestion.add(data)
+                }
+
+                val listData: ObservableList<String> = FXCollections.observableArrayList(listQuestion)
+                su_question.items = listData
+
+            }
+
+            fun switchForm(event: ActionEvent) {
+                val slider = TranslateTransition()
+
+                if (event.source == side_CreateBtn) {
+                    slider.node = side_form
+                    slider.toX = 300.0
+                    slider.duration = Duration.seconds(0.5)
+
+                    slider.setOnFinished {
+                        side_alreadyHave?.isVisible = true
+                        side_CreateBtn?.isVisible = false
+                        AnchorPane.setRightAnchor(side_form, 300.0)
+
+                        regQuestionList();
+                    }
+
+                    slider.play()
+                } else if (event.source == side_alreadyHave) {
+                    slider.node = side_form
+                    slider.toX = 0.0
+                    slider.duration = Duration.seconds(0.5)
+
+                    slider.setOnFinished {
+                        side_alreadyHave?.isVisible = false
+                        side_CreateBtn?.isVisible = true
+                        AnchorPane.setRightAnchor(side_form, null)
+                    }
+
+                    slider.play()
+                }
+            }
 }
