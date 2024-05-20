694b74546e5bb2200c51c52c9dfa559979f3e57a
JasonSX1 <geisonyt3@gmail.com>
2024-05-19 21:36:02 -0300

    Atualização UI

--- CinemaManagementSystem/src/main/kotlin/com/ed/cinemamanagementsystem/MainFormController.kt
+++ CinemaManagementSystem/src/main/kotlin/com/ed/cinemamanagementsystem/MainFormController.kt
@@ -1,4 +1,10 @@
-package com.ed.cinemamanagementsystem
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.ResourceBundle
 
-class MainFormController {
+class MainFormController : Initializable {
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        // Inicialização do controlador
+    }
 }
