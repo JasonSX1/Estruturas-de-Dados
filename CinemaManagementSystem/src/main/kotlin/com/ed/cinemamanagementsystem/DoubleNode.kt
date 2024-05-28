a211e6f37d290ea6f0015fd1c6f7fa2502a23748
Geison de Oliveira <geisonyt3@gmail.com>
2024-05-28 12:25:59 -0300

    Correção do tipo de dado da classe doublenode

--- CinemaManagementSystem/src/main/kotlin/com/ed/cinemamanagementsystem/DoubleNode.kt
+++ CinemaManagementSystem/src/main/kotlin/com/ed/cinemamanagementsystem/DoubleNode.kt
@@ -1,6 +1,6 @@
 package com.ed.cinemamanagementsystem
 
-data class DoubleNode (var dado: Any? = null) {
+data class DoubleNode(var movie: Movie) {
     var previous: DoubleNode? = null
     var next: DoubleNode? = null
 }
