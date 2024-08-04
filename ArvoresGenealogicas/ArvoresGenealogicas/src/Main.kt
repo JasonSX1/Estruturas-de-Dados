d6c54cb9de187c96bd5550003d556047ee816e78
Igor de eça almeida <igordrillpsn@gmail.com>
2024-08-04 01:15:03 -0300

    chama papa

--- ArvoresGenealogicas/ArvoresGenealogicas/src/Main.kt
+++ ArvoresGenealogicas/ArvoresGenealogicas/src/Main.kt
@@ -1,3 +1,52 @@
 fun main() {
-    println("Hello World!")
+    println("Bem-vindo ao sistema de árvores genealógicas!")
+    println("O que você deseja fazer?")
+    println("1 - Criar uma nova árvore genealógica(uma por vez)")
+    println("2 - Adicionar uma pessoa à árvore genealógica")
+    println("3 - Remover uma pessoa da árvore genealógica")
+    println("4 - Buscar uma pessoa na árvore genealógica")
+    println("5 - Imprimir a árvore genealógica")
+    println("6 - Limpar a árvore genealógica")
+    println("7 - Sair")
+    var escolha = readLine()!!.toInt()
+    val listaAux = ArvoreGenealogica()
+    when (escolha) {
+        1 -> {
+            println("Digite o nome da pessoa que irá ser a raiz: ")
+            val raiz = readLine()
+            println("Digite a data de nascimento da pessoa que irá ser a raiz: ")
+            val dataNasc = readLine()!!.toInt()
+            println("A pessoa está morta? (S/N)")
+            val morta = readLine()
+            var dataFal: Int? = null
+            if (morta == "S") {
+                println("Digite a data de falecimento da pessoa que irá ser a raiz(): ")
+                dataFal = readLine()!!.toInt()
+            }
+            listaAux.criarRaiz(raiz, dataNasc, dataFal)
+
+        }
+        2 -> {
+            println("Digite o nome da pessoa que deseja adicionar:")
+        }
+        3 -> {
+            println("Digite o nome da pessoa que deseja adicionar:")
+        }
+        4 -> {
+            println("Digite o nome da pessoa que deseja adicionar:")
+        }
+        5 -> {
+            println("Digite o nome da pessoa que deseja adicionar:")
+        }
+        6 -> {
+            println("Digite o nome da pessoa que deseja adicionar:")
+        }
+        7 -> {
+            println("Digite o nome da pessoa que deseja adicionar:")
+        }
+        default -> {
+            println("Digite o nome da pessoa que deseja adicionar:")
+        }
+    }
+
 }
